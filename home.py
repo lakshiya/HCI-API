ae22685110555159a494381b4f956665ab0d2973
Priya Lakshmi T <priyalakshmi02121997@gmail.com>
2023-04-25 12:07:14 -0400

    chagne

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import jsonify
 home_bp = Blueprint('home', __name__)
 
-@home_bp.route('/hello/')
+@home_bp.route('/hello/', methods = ['GET'])
 def hello():
     return jsonify({'message':"Hello from Home Page, this is response from FLASK",
                     'extra':'India'}) 
