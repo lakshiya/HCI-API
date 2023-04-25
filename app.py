ea1a2f572598102a16f69d8692461671640374db
Priya Lakshmi T <priyalakshmi02121997@gmail.com>
2023-04-25 00:32:31 -0400

    first commit

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(home_bp, url_prefix='/home')
 app.register_blueprint(contact_bp, url_prefix='/contact')
 
-app.run()
+
+if __name__ == '__main__':
+    app.run(host='priyalakshmi12.pythonanywhere.com',port=5000)
+    # app.run(host='0.0.0.0', port=105)
