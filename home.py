5556a29abe80f1c32f42fd65f98804b5d0864fac
Priya Lakshmi T <priyalakshmi02121997@gmail.com>
2023-04-25 13:19:53 -0400

    chagne

--- home.py
+++ home.py
@@ -4,5 +4,5 @@ home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/hello/', methods = ['GET'])
 def hello():
-    return jsonify({'message':"Hello from Home Page, this is response from FLASK",
-                    'extra':'India'}) 
+    return jsonify({"message":"Hello from Home Page, this is response from FLASK",
+                    "extra":"India"}) 
